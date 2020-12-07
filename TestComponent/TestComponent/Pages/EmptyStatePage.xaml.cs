1990c81949b522eb05ff07bd1a903d900254656b
Marthin Silalahi <marthin.silalahi@anacle.com>
2020-12-08 01:38:54 +0800

    Add sample mvvm pattern for empty state page

--- TestComponent/TestComponent/Pages/EmptyStatePage.xaml.cs
+++ TestComponent/TestComponent/Pages/EmptyStatePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TestComponent.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,8 @@ namespace TestComponent.Pages
         public EmptyStatePage()
         {
             InitializeComponent();
+
+            BindingContext = new EmptyStateViewModel();
         }
     }
 }
---
+++ TestComponent/TestComponent/ViewModels/EmptyStateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TestComponent.ViewModels
+{
+    public class EmptyStateViewModel : BaseViewModel
+    {
+        public ICommand ImageRetryCommand => new Command(ImageRetry);
+        public ICommand RetryCommand => new Command(Retry);
+
+        public EmptyStateViewModel()
+        {
+
+        }
+
+        private async void ImageRetry()
+        {
+            await App.Current.MainPage.DisplayAlert("Image tapped", "Please wait...", "OK");
+        }
+
+        private async void Retry()
+        {
+            await App.Current.MainPage.DisplayAlert("Action", "Please wait...", "OK");
+        }
+    }
+}
